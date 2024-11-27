7c1c35e2281ff52d4fa9fb2d131f0287b1dfa11c
ValCubeta <valcubetaa@gmail.com>
2024-11-27 20:18:50 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-#[allow(unused_imports)]
 use std::net::TcpListener;
+use std::io;
+use std::io::Write;
 
-fn main() {
+const RES_200: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
+
+fn main() -> io::Result<()> {
   println!("Logs from your program will appear here!");
 
   let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
   for stream in listener.incoming() {
     match stream {
-      Ok(_stream) => {
+      Err(why) => println!("Error: {why}"),
+      Ok(mut stream) => {
         println!("Accepted new connection");
-      }
-      Err(why) => {
-        println!("Error: {why}");
+        stream.write_all(RES_200)?;
+        stream.flush()?;
       }
     }
   }
+
+  Ok(())
 }
